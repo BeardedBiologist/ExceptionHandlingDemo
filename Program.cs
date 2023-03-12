8608f412a4517e7953a33ce17ff3cc227dcb378a
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-03-12 10:50:51 +0100

    Setup basuc error handling

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
-﻿namespace ConsoleUI
+﻿namespace ExceptionHandlingDemo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int[] ages = new int[] { 1, 3, 5 };
+
+            for (int i = 0; i <= ages.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine(ages[i]);
+                }
+                catch
+                {
+                    Console.WriteLine("We had an error, try again");
+                }
+            }
 
             Console.ReadLine();
         }
